c1ea30e8cf5341af34a4acf30a58cb654e8d80e8
Lemon196 <hrut196@163.com>
2019-01-12 14:23:02 +0800

    Initial commit

---
+++ src/main/java/cn/appsys/dao/DevUserMapper.java
@@ -0,0 +1,5 @@
+package cn.appsys.dao;
+
+public class DevUserMapper {
+
+}
---
+++ src/main/java/cn/appsys/pojo/AdPromotion.java
@@ -0,0 +1,107 @@
+package cn.appsys.pojo;
+
+
+import java.util.Date;
+
+public class AdPromotion {
+
+  private Long id;
+  private Long appId;
+  private String adPicPath;
+  private Long adPv;
+  private Long carouselPosition;
+  private Date startTime;
+  private Date endTime;
+  private Long createdBy;
+  private Date creationDate;
+  private Long modifyBy;
+  private Date modifyDate;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public Long getAppId() {
+    return appId;
+  }
+
+  public void setAppId(Long appId) {
+    this.appId = appId;
+  }
+
+  public String getAdPicPath() {
+    return adPicPath;
+  }
+
+  public void setAdPicPath(String adPicPath) {
+    this.adPicPath = adPicPath;
+  }
+
+  public Long getAdPv() {
+    return adPv;
+  }
+
+  public void setAdPv(Long adPv) {
+    this.adPv = adPv;
+  }
+
+  public Long getCarouselPosition() {
+    return carouselPosition;
+  }
+
+  public void setCarouselPosition(Long carouselPosition) {
+    this.carouselPosition = carouselPosition;
+  }
+
+  public Date getStartTime() {
+    return startTime;
+  }
+
+  public void setStartTime(Date startTime) {
+    this.startTime = startTime;
+  }
+
+  public Date getEndTime() {
+    return endTime;
+  }
+
+  public void setEndTime(Date endTime) {
+    this.endTime = endTime;
+  }
+
+  public Long getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(Long createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public Date getCreationDate() {
+    return creationDate;
+  }
+
+  public void setCreationDate(Date creationDate) {
+    this.creationDate = creationDate;
+  }
+
+  public Long getModifyBy() {
+    return modifyBy;
+  }
+
+  public void setModifyBy(Long modifyBy) {
+    this.modifyBy = modifyBy;
+  }
+
+  public Date getModifyDate() {
+    return modifyDate;
+  }
+
+  public void setModifyDate(Date modifyDate) {
+    this.modifyDate = modifyDate;
+  }
+}
